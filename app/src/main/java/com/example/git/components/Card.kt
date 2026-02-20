44b6ed922487bae3ebad726b4341811e1fea87e9
Olesya <olesya.lekareva@osinit.com>
2026-02-20 21:21:41 +0300

    recode Card

--- app/src/main/java/com/example/git/components/Card.kt
+++ app/src/main/java/com/example/git/components/Card.kt
@@ -1,7 +1,66 @@
 package com.example.git.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun Card () {
+fun Card(
+    modifier: Modifier,
+    title: String,
+    description: String,
+    int: Int = 16
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .clip(shape = RoundedCornerShape(int.dp))
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .background(Color.Transparent)
+
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(top = 30.dp, bottom = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                lineHeight = 48.sp,
+                softWrap = true,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = description,
+                fontSize = 15.sp,
+                modifier = modifier.padding(bottom = 15.dp),
+                textAlign = TextAlign.Center,
+            )
+        }
+
+    }
 }

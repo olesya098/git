3165a0e4e2ae49ba4124e35fc31d4362977de10c
Olesya <olesya.lekareva@osinit.com>
2026-02-20 21:07:18 +0300

    created Home

---
+++ app/src/main/java/com/example/git/screen/Home.kt
@@ -0,0 +1,25 @@
+package com.example.git.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Home() {
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.background
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(innerPadding)
+        ) {
+        }
+    }
+}
